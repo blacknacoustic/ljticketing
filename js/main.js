abd389260a6682944b8a11085ed935d36d0f3c77
blacknacoustic <66539747+blacknacoustic@users.noreply.github.com>
2023-02-03 10:55:44 -0500

    uploading initial files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("newCus").addEventListener("click", function(){
+    window.location.href = "newcustomer.html";
+});
+
+document.getElementById("oldCus").addEventListener("click", function(){
+    window.location.href = "oldcustomer.html";
+});
