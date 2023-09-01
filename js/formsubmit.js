6691454b92efbf66bc789940f6ab4fa625af556d
David Jolly <dj2099113@gmail.com>
2023-09-01 10:55:50 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .DS_Store
    	modified:   js/formsubmit.js
    	modified:   newcustomer.html

--- js/formsubmit.js
+++ js/formsubmit.js
@@ -1,76 +1,26 @@
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCW5l3tWQaQs5on6JXrApcxJhreYBuN1LY",
-//   authDomain: "ljelectronics-e94fb.firebaseapp.com",
-//   databaseURL: "https://ljelectronics-e94fb-default-rtdb.firebaseio.com",
-//   projectId: "ljelectronics-e94fb",
-//   storageBucket: "ljelectronics-e94fb.appspot.com",
-//   messagingSenderId: "389542626733",
-//   appId: "1:389542626733:web:640c57e9a28191a51c0d24",
-//   measurementId: "G-JMNYRPJBFS"
-// };
+// formSubmit.js
 
-// // Initialize Firebase
-// initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const db = firebase.database();
-
-
-// Reference to the form
-const form = document.getElementById("myForm");
-
-// Reference to the Firebase database
-const database = firebase.database();
-const entryNumberRef = database.ref("entryNumber");
-
-let latestEntryNumber = 0;
-entryNumberRef.once("value", snapshot => {
-  latestEntryNumber = snapshot.val() || 0;
-});
-
-// Listen for form submission
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  // Get form values
-  const name = form.name.value;
-  const date = form.date.value;
-  const number = form.number.value;
-  const modelnum = form.modelnum.value;
-  const serialnum = form.serialnum.value;
-  const description = form.description.value;
-  const items = form.items.value;
-
-  // Parse the date to get year and month
-  const selectedDate = new Date(date);
-  const year = selectedDate.getFullYear();
-  const month = selectedDate.getMonth() + 1; // JavaScript months are zero-based
-
-  // Generate a unique entry number
-  latestEntryNumber++;
-  const entryCode = `CR${latestEntryNumber.toString().padStart(6, "0")}`;
-
-  // Update the entry number in the database
-  entryNumberRef.set(latestEntryNumber);
-
-  // Create an object with form data
-  const formData = {
-    name: name,
-    date: date,
-    number: number,
-    modelnum: modelnum,
-    serialnum: serialnum,
-    description: description,
-    items: items,
-    entryCode: entryCode
-  };
-
-  // Push form data to the database under year and month
-  database.ref("repairForms").child(year).child(month).push(formData, function(error) {
-    if (error) {
-      console.error("Error saving data:", error);
-    } else {
-      console.log("Data saved successfully!");
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('myForm');
+  form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const jsonData = {};
+    formData.forEach((value, key) => {
+      jsonData[key] = value;
+    });
+    const response = await fetch('https://script.google.com/macros/s/AKfycbzC7BglF_aTJrKj5BuUUXJ-vmSgSS3fWFsmbCmQMb4T4nySOxElUbsfOW5lFr2ppPil/exec', {
+      method: 'POST',
+      body: JSON.stringify(jsonData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      alert('Form submitted successfully');
       form.reset();
+    } else {
+      alert('Form submission failed');
     }
   });
 });
