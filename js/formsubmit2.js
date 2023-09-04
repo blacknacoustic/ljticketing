6cd2df7dc96cce9f39b35af48358672e2905066d
blacknacoustic <66539747+blacknacoustic@users.noreply.github.com>
2023-09-04 13:11:15 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   js/formsubmit2.js
    	modified:   js/notionform.js
    	modified:   newcustomer.html

--- js/formsubmit2.js
+++ js/formsubmit2.js
@@ -1,3 +1,10 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js'
+// If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.3.1/firebase-analytics.js'
+// Add Firebase products that you want to use
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js'
+
 const firebaseConfig = {
   apiKey: "AIzaSyCW5l3tWQaQs5on6JXrApcxJhreYBuN1LY",
   authDomain: "ljelectronics-e94fb.firebaseapp.com",
@@ -5,21 +12,24 @@ const firebaseConfig = {
   projectId: "ljelectronics-e94fb",
   storageBucket: "ljelectronics-e94fb.appspot.com",
   messagingSenderId: "389542626733",
-  appId: "1:389542626733:web:640c57e9a28191a51c0d24",
-  measurementId: "G-JMNYRPJBFS"
+  appId: "1:389542626733:web:e8a94549e03f63281c0d24",
+  measurementId: "G-SC116PD2GD"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Reference to the form
 const form = document.getElementById("myForm");
   
 // Reference to the Firestore database
-const db = firebase.firestore();
+const db = getFirestore(app);
 
 // Listen for form submission
+// Assuming your Firebase and Firestore have been initialized correctly
+
+// Add an event listener to the form
 form.addEventListener("submit", async function (event) {
-  event.preventDefault();
+  event.preventDefault(); // Prevent the default form submission behavior
 
   // Get form values
   const name = form.name.value;
@@ -30,35 +40,40 @@ form.addEventListener("submit", async function (event) {
   const description = form.description.value;
   const items = form.items.value;
 
-  // Parse the date to get year and month
-  const selectedDate = new Date(date);
-  const year = selectedDate.getFullYear();
-  const month = selectedDate.getMonth() + 1; // JavaScript months are zero-based
+  try {
+    // Parse the date to get year and month
+    const selectedDate = new Date(date);
+    const year = selectedDate.getFullYear();
+    const month = selectedDate.getMonth() + 1; // JavaScript months are zero-based
 
-  // Generate a unique entry number
-  const entrySnapshot = await db.collection("entryNumber").doc("latest").get();
-  let latestEntryNumber = entrySnapshot.exists ? entrySnapshot.data().number : 0;
-  latestEntryNumber++;
-  const entryCode = `CR${latestEntryNumber.toString().padStart(6, "0")}`;
+    // Generate a unique entry number
+    const entrySnapshot = await db.collection("entryNumber").doc("latest").get();
+    let latestEntryNumber = entrySnapshot.exists ? entrySnapshot.data().number : 0;
+    latestEntryNumber++;
+    const entryCode = `CR${latestEntryNumber.toString().padStart(6, "0")}`;
 
-  // Update the entry number in Firestore
-  await db.collection("entryNumber").doc("latest").set({ number: latestEntryNumber });
+    // Update the entry number in Firestore
+    await db.collection("entryNumber").doc("latest").set({ number: latestEntryNumber });
 
-  // Create an object with form data
-  const formData = {
-    name: name,
-    date: date,
-    number: number,
-    modelnum: modelnum,
-    serialnum: serialnum,
-    description: description,
-    items: items,
-    entryCode: entryCode,
-  };
+    // Create an object with form data
+    const formData = {
+      name: name,
+      date: date,
+      number: number,
+      modelnum: modelnum,
+      serialnum: serialnum,
+      description: description,
+      items: items,
+      entryCode: entryCode,
+    };
 
-  // Push form data to Firestore
-  await db.collection("repairForms").doc(year.toString()).collection(month.toString()).add(formData);
+    // Push form data to Firestore
+    await db.collection("repairForms").doc(year.toString()).collection(month.toString()).add(formData);
 
-  console.log("Data saved successfully!");
-  form.reset();
+    console.log("Data saved successfully!");
+    form.reset(); // Reset the form after successful submission
+  } catch (error) {
+    console.error("Error:", error); // Handle any potential errors
+  }
 });
+
